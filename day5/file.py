5485969141c47977a2ac89568ce224a2e68f18e0
sharma <iluvcinemaofficial@gmail.com>
2023-12-22 23:53:35 +0530

    updated day5

---
+++ day5/file.py
@@ -0,0 +1,6 @@
+test_list = input("Please provide the elements:").split()
+print(test_list)
+
+# To convert the input values into a tuple instead of converting it into a list. Use the following method :
+elements = tuple(input("Please provide the elements to convert into a list:").split())
+print(elements)
